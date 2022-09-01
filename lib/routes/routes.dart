4633fbe37e5839d9c1fe50960b44f82008ba71f7
redemtiom <smar.pre.sky@gmail.com>
2022-08-31 22:36:28 -0500

    first commit, added needed pages, added routes

---
+++ lib/routes/routes.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+import 'package:chat/pages/chat_page.dart';
+import 'package:chat/pages/loading_page.dart';
+import 'package:chat/pages/login_page.dart';
+import 'package:chat/pages/register_page.dart';
+import 'package:chat/pages/users_page.dart';
+
+final Map<String, Widget Function(BuildContext)> appRoutes = {
+  'users':    (_) => const UsersPage(),
+  'chat':     (_) => const ChatPage(),
+  'login':    (_) => const LoginPage(),
+  'register': (_) => const RegisterPage(),
+  'loading':  (_) => const LoadingPage()
+};
